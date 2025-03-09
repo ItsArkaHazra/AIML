c1dbb8551dcd6f0f0c6972d8825c09eeffdee7a6
Arka1905 <arkahazrajob@gmail.com>
2025-03-09 23:22:47 +0530

    1st practice

---
+++ Python-Basics/MathematicalOperation.py
@@ -0,0 +1,7 @@
+a=int(input("Enter 1st Number"))
+b=int(input("Enter 2nd Number"))
+print("Addition of ",a," and addition of b",b,"is:: ", a+b)
+print("Subtraction of ",a," and subtraction of b",b,"is:: ", a-b)
+print("Multiplicaiton of ",a," and multiplication of b",b,"is:: ", a*b)
+print("Division of ",a," and division of b",b,"is:: ", a/b)
+
